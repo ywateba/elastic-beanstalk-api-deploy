f9c5152033c0b420d19d85c66e9a8f4d1e03a3aa
ywateba <yao.wateba@gmail.com>
2024-02-19 12:29:19 +0100

    rewrite util for async call
    modified filterImageUrl to handle async call correctly and return output path

--- util/util.js
+++ util/util.js
@@ -10,22 +10,24 @@ import Jimp from "jimp";
 // RETURNS
 //    an absolute path to a filtered image locally saved file
  export async function filterImageFromURL(inputURL) {
-  return new Promise(async (resolve, reject) => {
     try {
+      const outpath = "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+
+      console.log(outpath)
       const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(outpath, (img) => {
-          resolve(outpath);
-        });
+        .writeAsync(outpath)
+
+      return outpath
     } catch (error) {
-      reject(error);
+      console.log(error)
+
     }
-  });
+
 }
 
 // deleteLocalFiles

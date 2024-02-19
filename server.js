f0244e1b46a59286e502bb8533f84a266aa88dd5
ywateba <yao.wateba@gmail.com>
2024-02-19 12:32:18 +0100

    added filtered image endpoint

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import validator from 'validator'
+
 import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
 
 
@@ -9,34 +11,42 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-    /**************************************************************************** */
-
-  //! END @TODO1
   
+
+
+
+  app.get( "/filteredimage/:image_url", async (req, res) => {
+
+    const { image_url } = req.params;
+    console.log(image_url)
+
+    // Validate the URL
+    if (!image_url || !validator.isURL(image_url)) {
+      return res.status(400).send({ message: "The image_url provided is not a valid URL." });
+    }
+    let saved_url = await filterImageFromURL(image_url)
+    console.log(saved_url)
+    res.sendFile(saved_url,(err) => {
+        if (err) {
+          console.log(err)
+          res.status = 500
+
+        }else{
+          console.log("File sent successful")
+        }
+    })
+  } );
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
